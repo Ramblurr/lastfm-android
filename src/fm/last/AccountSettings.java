3ba73f102bd9f9475f9b2c774b6d8dc61e5f96b3
Mike Jennings <jennings@google.com>
2008-10-21 16:34:07 +0000

    Tweaked AuthenticationTask
    git-svn-id: https://android-lastfm.googlecode.com/svn/trunk@35 a9466a9e-ad51-0410-a212-537f35f33a4e

--- src/fm/last/AccountSettings.java
+++ src/fm/last/AccountSettings.java
@@ -14,17 +14,22 @@ import android.widget.EditText;
 import android.util.Log;
 import android.view.View;
 import android.os.Handler;
+import androidx.util.GUITaskQueue;
+import androidx.util.ResultReceiver;
 
 
 import fm.last.Utils; 
 import fm.last.radio.RadioHandshake;
+import fm.last.tasks.AuthenticationTask;
 
 
-public class AccountSettings extends Activity 
+public class AccountSettings extends Activity implements ResultReceiver<RadioHandshake>
 {
 	private EditText userField, passwordField;
 	private final Handler m_handler = new Handler();
 	ProgressDialog m_progress = null;
+	private String md5Password;
+	private String username;
 
 	public void onCreate(Bundle icicle)
 	{
@@ -41,15 +46,25 @@ public class AccountSettings extends Activity
 
 	private View.OnClickListener onLoginClick = new View.OnClickListener() {
 		public void onClick(View v) {
-			m_progress = ProgressDialog.show(AccountSettings.this,
-					getResources().getString(R.string.authProgressTitle),
-					getResources().getString(R.string.authProgressMessage),
-					true);
-
-			new Thread(runAuthentication).start();
+		  doLogin();
 		}
 	};
 
+	private void doLogin() {
+      m_progress = ProgressDialog.show(AccountSettings.this,
+          getResources().getString(R.string.authProgressTitle),
+          getResources().getString(R.string.authProgressMessage),
+          true);
+      username = userField.getText().toString();
+      md5Password = Utils.md5(passwordField.getText().toString());
+      
+      GUITaskQueue.getInstance().addTask(
+      new AuthenticationTask(username, md5Password, m_progress, this));
+//new Thread(runAuthentication).start();
+	  
+	}
+	
+	
 	Runnable runAuthentication = new Runnable() {
 		private RadioHandshake m_loginTest;
 
@@ -89,4 +104,30 @@ public class AccountSettings extends Activity
 						   .show();
 		}
 	};
+
+  public void handle_exception(Throwable t) {
+    new AlertDialog.Builder( AccountSettings.this )
+    .setTitle( R.string.badAuthTitle )
+    .setIcon( R.drawable.icon )
+    .setMessage( R.string.badAuth )
+    .show();
+  }
+
+  public void resultObtained(RadioHandshake m_loginTest) {
+    if (!m_loginTest.isValid()) {
+      handle_exception(null);
+    } else {
+      SharedPreferences prefs = AccountSettings.this
+              .getSharedPreferences("Last.fm", Context.MODE_PRIVATE);
+
+      SharedPreferences.Editor prefEdit = prefs.edit();
+
+      prefEdit.putString("username", username);
+      prefEdit.putString("md5Password", md5Password);
+
+      prefEdit.commit();
+
+      AccountSettings.this.finish();
+    }
+  }
 }
--- src/fm/last/tasks/AuthenticationTask.java
+++ src/fm/last/tasks/AuthenticationTask.java
@@ -3,28 +3,28 @@ package fm.last.tasks;
 import fm.last.Utils;
 import fm.last.radio.RadioHandshake;
 import android.app.ProgressDialog;
+import android.view.View.OnClickListener;
 import android.widget.EditText;
 import androidx.util.GUITask;
+import androidx.util.ResultReceiver;
 
 public class AuthenticationTask implements GUITask {
-  private EditText userField;
-  private EditText passwordField;
+  private String username;
+  private String md5Password;
   private ProgressDialog m_progress;
-  private String userName;
-  private String userPassword;
   private RadioHandshake m_loginTest;
+  private ResultReceiver<RadioHandshake> resultReceiver;
 
-  public AuthenticationTask(EditText userField, EditText passwordField, 
-      ProgressDialog m_progress) {
-    this.userField = userField;
-    this.passwordField = passwordField;
+  public AuthenticationTask(String username, String md5Password, 
+      ProgressDialog m_progress, ResultReceiver<RadioHandshake> resultReceiver) {
+    this.username = username;
+    this.md5Password = md5Password;
     this.m_progress = m_progress;
-    userName = userField.getText().toString();
-    userPassword = Utils.md5(passwordField.getText().toString());
+    this.resultReceiver = resultReceiver;
   }
   
   public void executeNonGuiTask() throws Exception {
-    m_loginTest = new RadioHandshake(userName, userPassword);
+    m_loginTest = new RadioHandshake(username, md5Password);
     m_loginTest.connect();
   }
 
