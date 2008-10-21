996dc538a6fbb959b4923566a26d12f7908b0358
Mike Jennings <jennings@google.com>
2008-10-21 17:07:00 +0000

    Started fixing RadioHandshake
    git-svn-id: https://android-lastfm.googlecode.com/svn/trunk@38 a9466a9e-ad51-0410-a212-537f35f33a4e

--- src/fm/last/radio/RadioHandshake.java
+++ src/fm/last/radio/RadioHandshake.java
@@ -1,10 +1,13 @@
 package fm.last.radio;
 
+import fm.last.Log;
+
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.util.HashMap;
-import android.util.Log;
 
 public class RadioHandshake {
 	private String m_userName, m_md5Password = null;
@@ -15,41 +18,31 @@ public class RadioHandshake {
 		m_md5Password = md5Password;
 	}
 
-	public void connect() {
-//		Log.i("Last.fm", "Handshaking with Last.fm radio");
-//		URL url = null;
-//		try {
-//			String urlString;
-//			urlString = "http://ws.audioscrobbler.com/radio/handshake.php?version=0.1&platform=android&platformversion=beta&username=";
-//			urlString += m_userName;
-//			urlString += "&passwordmd5=";
-//			urlString += m_md5Password;
-//			urlString += "&language=en";
-//			url = new URL(urlString);
-//		} catch (java.net.MalformedURLException e) {
-//			Log.i("Last.fm", "Error: Malformed URL");
-//			return;
-//		}
-//
-//		try {
-//			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//			conn.setRequestMethod("GET");
-//			InputStream inp = conn.getInputStream();
-//			String content = new String();
-//			while (inp.available() > 0) {
-//				content += ((char) inp.read());
-//			}
-//			String params[] = content.split("\n");
-//			for (String param : params) {
-//				String curParam[] = param.split("=", 2);
-//				m_parameters.put(curParam[0],
-//						(curParam.length > 1) ? curParam[1] : "");
-//				Log.i("Last.fm", "parameter[" + curParam[0] + "] = "
-//						+ ((curParam.length > 1) ? curParam[1] : ""));
-//			}
-//		} catch (java.io.IOException e) {
-//			Log.i("Last.fm", "Error: IOException: " + e);
-//		}
+	public void connect() throws IOException {
+		Log.i("Handshaking with Last.fm radio");
+		URL url = null;
+		String urlString;
+		urlString = "http://ws.audioscrobbler.com/radio/handshake.php?version=0.1&platform=android&platformversion=beta&username=";
+		urlString += m_userName;
+		urlString += "&passwordmd5=";
+		urlString += m_md5Password;
+		urlString += "&language=en";
+		url = new URL(urlString);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		InputStream inp = conn.getInputStream();
+		String content = new String();
+		while (inp.available() > 0) {
+			content += ((char) inp.read());
+		}
+		String params[] = content.split("\n");
+		for (String param : params) {
+			String curParam[] = param.split("=", 2);
+			m_parameters.put(curParam[0],
+					(curParam.length > 1) ? curParam[1] : "");
+			Log.i("parameter[" + curParam[0] + "] = "
+						+ ((curParam.length > 1) ? curParam[1] : ""));
+		}
 	}
 
 	public boolean isValid() {
