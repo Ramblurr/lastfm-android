1560f97e048b4950246322ffa88b45d1b97a31e7
Mike Jennings <jennings@google.com>
2008-10-23 10:42:16 +0000

    Refactored AuthenticationTask and AccountSettings
    git-svn-id: https://android-lastfm.googlecode.com/svn/trunk@44 a9466a9e-ad51-0410-a212-537f35f33a4e

--- src/fm/last/AccountSettings.java
+++ src/fm/last/AccountSettings.java
@@ -1,10 +1,8 @@
 package fm.last;
 
-import java.lang.Runnable;
-import java.lang.Thread;
-
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -17,17 +15,15 @@ import androidx.util.GUITaskQueue;
 import androidx.util.ResultReceiver;
 
 
-import fm.last.Utils;
 import fm.last.api.MD5;
 import fm.last.api.Session;
-import fm.last.radio.RadioHandshake;
 import fm.last.tasks.AuthenticationTask;
 
 
 public class AccountSettings extends Activity implements ResultReceiver<Session>
 {
 	private EditText userField, passwordField;
-	private final Handler m_handler = new Handler();
+//	private final Handler m_handler = new Handler();
 	ProgressDialog m_progress = null;
 	private String md5Password;
 	private String username;
@@ -41,15 +37,12 @@ public class AccountSettings extends Activity implements ResultReceiver<Session>
 		userField = (EditText) findViewById(R.id.login_username);
 		passwordField = (EditText) findViewById(R.id.login_password);
 
-		Log.i("Handler object: " + m_handler.toString());
-		loginButton.setOnClickListener(onLoginClick);
+		loginButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+			  doLogin();
+			}
+		});
 	}
-
-	private View.OnClickListener onLoginClick = new View.OnClickListener() {
-		public void onClick(View v) {
-		  doLogin();
-		}
-	};
 
 	private void doLogin() {
       m_progress = ProgressDialog.show(AccountSettings.this,
@@ -59,32 +52,32 @@ public class AccountSettings extends Activity implements ResultReceiver<Session>
       username = userField.getText().toString();
       md5Password = MD5.getInstance().hash(passwordField.getText().toString());
       GUITaskQueue.getInstance().addTask(
-      new AuthenticationTask(username, md5Password, m_progress, this));
+      new AuthenticationTask(username, md5Password, this));
 	}
 	
 
   public void handle_exception(Throwable t) {
-    new AlertDialog.Builder( AccountSettings.this )
+    m_progress.dismiss();  	
+    Dialog alert = (new AlertDialog.Builder( AccountSettings.this )
     .setTitle( R.string.badAuthTitle )
     .setIcon( R.drawable.icon )
     .setMessage( R.string.badAuth )
-    .show();
+    ).create();
+    alert.show();
+  }
+  
+  private SharedPreferences getPrivatePreferences() {
+  	return getSharedPreferences("Last.fm", Context.MODE_PRIVATE);
   }
 
   public void resultObtained(Session session) {
-  	Log.i("We've got a session! session=" + session);
-  	
-/*      
-      SharedPreferences prefs = AccountSettings.this
-              .getSharedPreferences("Last.fm", Context.MODE_PRIVATE);
-
+    m_progress.dismiss();  	
+  	Log.i("We've got a session! session.key=" + session.getKey());
+      SharedPreferences prefs = getPrivatePreferences();
       SharedPreferences.Editor prefEdit = prefs.edit();
-
       prefEdit.putString("username", username);
       prefEdit.putString("md5Password", md5Password);
-
       prefEdit.commit();
-      AccountSettings.this.finish();
-*/
+      this.finish();
   }
 }
--- src/fm/last/tasks/AuthenticationTask.java
+++ src/fm/last/tasks/AuthenticationTask.java
@@ -1,30 +1,23 @@
 package fm.last.tasks;
 
-import fm.last.Utils;
 import fm.last.android.AndroidLastFmServerFactory;
 import fm.last.api.LastFmServer;
 import fm.last.api.MD5;
 import fm.last.api.Session;
-import fm.last.radio.RadioHandshake;
-import android.app.ProgressDialog;
-import android.view.View.OnClickListener;
-import android.widget.EditText;
+//import fm.last.radio.RadioHandshake;
 import androidx.util.GUITask;
 import androidx.util.ResultReceiver;
 
 public class AuthenticationTask implements GUITask {
   private String username;
   private String md5Password;
-  private ProgressDialog m_progress;
-  private RadioHandshake m_loginTest;
+//  private RadioHandshake m_loginTest;
   private ResultReceiver<Session> resultReceiver;
   private Session session;
 
-  public AuthenticationTask(String username, String md5Password, 
-      ProgressDialog m_progress, ResultReceiver<Session> resultReceiver) {
+  public AuthenticationTask(String username, String md5Password, ResultReceiver<Session> resultReceiver) {
     this.username = username;
     this.md5Password = md5Password;
-    this.m_progress = m_progress;
     this.resultReceiver = resultReceiver;
   }
   
@@ -32,19 +25,13 @@ public class AuthenticationTask implements GUITask {
     LastFmServer server = AndroidLastFmServerFactory.getServer();
     String authToken = MD5.getInstance().hash(username + md5Password);
     session = server.getMobileSession(username, authToken);
-    m_loginTest = new RadioHandshake(username, md5Password);
-    m_loginTest.connect();
   }
 
   public void handle_exception(Throwable t) {
-    m_progress.dismiss();
     resultReceiver.handle_exception(t);
   }
 
   public void after_execute() {
-    m_progress.dismiss();
     resultReceiver.resultObtained(session);
   }
-
- 
 }
