e8017b9de65e81dda4ae1d3a48b07dd661f9b474
Mike Jennings <jennings@google.com>
2008-10-23 11:55:44 +0000

    More refactoring: moved preferences stuff into LastFmApplication
    git-svn-id: https://android-lastfm.googlecode.com/svn/trunk@45 a9466a9e-ad51-0410-a212-537f35f33a4e

--- src/fm/last/AccountSettings.java
+++ src/fm/last/AccountSettings.java
@@ -4,80 +4,63 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
-import android.os.Handler;
 import androidx.util.GUITaskQueue;
 import androidx.util.ResultReceiver;
-
 
 import fm.last.api.MD5;
 import fm.last.api.Session;
 import fm.last.tasks.AuthenticationTask;
 
-
-public class AccountSettings extends Activity implements ResultReceiver<Session>
-{
+public class AccountSettings extends Activity implements
+		ResultReceiver<Session> {
 	private EditText userField, passwordField;
-//	private final Handler m_handler = new Handler();
+	// private final Handler m_handler = new Handler();
 	ProgressDialog m_progress = null;
 	private String md5Password;
 	private String username;
 
-	public void onCreate(Bundle icicle)
-	{
-		super.onCreate( icicle );
-		setContentView( R.layout.account_settings );
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.account_settings);
 
-		final Button loginButton = (Button) findViewById( R.id.login_button );
+		final Button loginButton = (Button) findViewById(R.id.login_button);
 		userField = (EditText) findViewById(R.id.login_username);
 		passwordField = (EditText) findViewById(R.id.login_password);
 
 		loginButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-			  doLogin();
+				doLogin();
 			}
 		});
 	}
 
 	private void doLogin() {
-      m_progress = ProgressDialog.show(AccountSettings.this,
-          getResources().getString(R.string.authProgressTitle),
-          getResources().getString(R.string.authProgressMessage),
-          true);
-      username = userField.getText().toString();
-      md5Password = MD5.getInstance().hash(passwordField.getText().toString());
-      GUITaskQueue.getInstance().addTask(
-      new AuthenticationTask(username, md5Password, this));
+		String title = getResources().getString(R.string.authProgressTitle);
+		String message = getResources().getString(R.string.authProgressMessage);
+		m_progress = ProgressDialog.show(AccountSettings.this, title, message, true);
+		username = userField.getText().toString();
+		md5Password = MD5.getInstance().hash(passwordField.getText().toString());
+		GUITaskQueue.getInstance().addTask(
+				new AuthenticationTask(username, md5Password, this));
 	}
-	
 
-  public void handle_exception(Throwable t) {
-    m_progress.dismiss();  	
-    Dialog alert = (new AlertDialog.Builder( AccountSettings.this )
-    .setTitle( R.string.badAuthTitle )
-    .setIcon( R.drawable.icon )
-    .setMessage( R.string.badAuth )
-    ).create();
-    alert.show();
-  }
-  
-  private SharedPreferences getPrivatePreferences() {
-  	return getSharedPreferences("Last.fm", Context.MODE_PRIVATE);
-  }
+	public void handle_exception(Throwable t) {
+		m_progress.dismiss();
+		Dialog alert = (new AlertDialog.Builder(AccountSettings.this).setTitle(
+				R.string.badAuthTitle).setIcon(R.drawable.icon)
+				.setMessage(R.string.badAuth)).create();
+		alert.show();
+	}
 
-  public void resultObtained(Session session) {
-    m_progress.dismiss();  	
-  	Log.i("We've got a session! session.key=" + session.getKey());
-      SharedPreferences prefs = getPrivatePreferences();
-      SharedPreferences.Editor prefEdit = prefs.edit();
-      prefEdit.putString("username", username);
-      prefEdit.putString("md5Password", md5Password);
-      prefEdit.commit();
-      this.finish();
-  }
+	public void resultObtained(Session session) {
+		m_progress.dismiss();
+		Log.i("We've got a session! session.key=" + session.getKey());
+		// Save our credentials to our SharedPreferences
+		LastFmApplication.instance().saveCredentials(username, md5Password);
+		this.finish();
+	}
 }
--- src/fm/last/LastFmApplication.java
+++ src/fm/last/LastFmApplication.java
@@ -2,81 +2,80 @@ package fm.last;
 
 import java.io.FileNotFoundException;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
-import androidx.util.GUITaskQueue;
 
-public class LastFmApplication extends android.app.Application
-{
-    static private LastFmApplication instance;
-    private String m_user;
-    private String m_pass;
-    private String m_sessionKey;
-    private SharedPreferences m_preferences;
-    private SQLiteDatabase m_db = null;
-    
-    public void onCreate()
-    {
-        m_preferences = getSharedPreferences( "Last.fm", MODE_PRIVATE );
-        m_user = m_preferences.getString( "username", "" );
-        m_pass = m_preferences.getString( "md5Password", "" );
-        m_sessionKey = m_preferences.getString( "sessionKey", "" );
-        
-        if( m_user.trim().length() == 0 )
-        {
-            Intent i = new Intent( "ACCOUNTSETTINGS" );
-            i.setFlags( Intent.FLAG_ACTIVITY_NEW_TASK );
-            startActivity( i );
-        }
-        
-        instance = this;
-    }
+public class LastFmApplication extends android.app.Application {
+	static private LastFmApplication instance;
+	private String m_user;
+	private String m_pass;
+	private String m_sessionKey;
+	private SharedPreferences m_preferences;
+	private SQLiteDatabase m_db = null;
 
-    public static LastFmApplication instance()
-    {
-        return instance;
-    }
+	public void onCreate() {
+		instance = this;
+		m_preferences = getPrivatePreferences();
+		m_user = m_preferences.getString("username", "");
+		m_pass = m_preferences.getString("md5Password", "");
+		m_sessionKey = m_preferences.getString("sessionKey", "");
 
-    
-    public SQLiteDatabase getDb() throws FileNotFoundException
-    {
-        if( m_db != null )
-        {
-            return m_db;
-        }
-        
-        m_db = openOrCreateDatabase( "lastFm", MODE_PRIVATE, null );
-        createTables();
-        return m_db;
+		if (m_user.trim().length() == 0) {
+			Intent i = new Intent("ACCOUNTSETTINGS");
+			i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(i);
+		}
+	}
 
-    }
-    
-    private void createTables()
-    {
-        m_db.execSQL( "CREATE TABLE FriendsMap (contactId integer PRIMARY_KEY, username VARCHAR);" );
-    }
-    
-    public String userName()
-    {
-        return m_user;
-    }
+	public SharedPreferences getPrivatePreferences() {
+		return getSharedPreferences("Last.fm", Context.MODE_PRIVATE);
+	}
 
-    public String sessionKey()
-    {
-        return m_sessionKey;
-    }
-    
-    public boolean setSessionKey( String sessionKey )
-    {
-        m_sessionKey = sessionKey;
-        return
-            m_preferences.edit().putString( "sessionKey", sessionKey )
-                                .commit();
-    }
+  public void saveCredentials(String username, String md5Password) {
+      SharedPreferences prefs = getPrivatePreferences();
+      SharedPreferences.Editor prefEdit = prefs.edit();
+      prefEdit.putString("username", username);
+      prefEdit.putString("md5Password", md5Password);
+      prefEdit.commit();
+  }
+	
+	
+	public static LastFmApplication instance() {
+		return instance;
+	}
 
-    public String password()
-    {
-        return m_pass;
-    }
+	public SQLiteDatabase getDb() throws FileNotFoundException {
+		if (m_db != null) {
+			return m_db;
+		}
+
+		m_db = openOrCreateDatabase("lastFm", MODE_PRIVATE, null);
+		createTables();
+		return m_db;
+
+	}
+
+	private void createTables() {
+		m_db
+				.execSQL("CREATE TABLE FriendsMap (contactId integer PRIMARY_KEY, username VARCHAR);");
+	}
+
+	public String userName() {
+		return m_user;
+	}
+
+	public String sessionKey() {
+		return m_sessionKey;
+	}
+
+	public boolean setSessionKey(String sessionKey) {
+		m_sessionKey = sessionKey;
+		return m_preferences.edit().putString("sessionKey", sessionKey).commit();
+	}
+
+	public String password() {
+		return m_pass;
+	}
 }
