1cd5f27e871f75465f944aaea5a6449b7b653934
Mike Jennings <jennings@google.com>
2008-10-25 18:05:33 +0000

    Added TuneRadioTask
    git-svn-id: https://android-lastfm.googlecode.com/svn/trunk@54 a9466a9e-ad51-0410-a212-537f35f33a4e

---
+++ src/fm/last/tasks/TuneRadioTask.java
@@ -0,0 +1,43 @@
+package fm.last.tasks;
+
+import fm.last.LastFmApplication;
+import fm.last.android.AndroidLastFmServerFactory;
+import fm.last.api.Artist;
+import fm.last.api.LastFmServer;
+import fm.last.api.Session;
+import fm.last.api.Station;
+import android.net.Uri;
+import androidx.util.GUITask;
+import androidx.util.ResultReceiver;
+
+public class TuneRadioTask implements GUITask {
+	private Artist artist;
+	private Station radioStation;
+	private LastFmServer server;
+	private ResultReceiver<Station> resultReceiver;
+	
+	private TuneRadioTask() {
+		server = AndroidLastFmServerFactory.getServer();
+	}
+	
+	public TuneRadioTask(Artist artist, ResultReceiver<Station> resultReceiver) {
+		this();
+		this.artist = artist;
+		this.resultReceiver = resultReceiver;
+	}
+	
+	public void executeNonGuiTask() throws Exception {
+		String station = "lastfm://artist/" + Uri.encode( artist.getName() ) + "/similarartists";
+		Session session = LastFmApplication.instance().getSession();
+		String sk = session.getKey();
+		radioStation = server.tuneToSimilarArtist(station, sk);
+	}
+
+	public void handle_exception(Throwable t) {
+		resultReceiver.handle_exception(t);
+	}
+
+	public void after_execute() {
+		resultReceiver.resultObtained(radioStation);
+	}
+}
