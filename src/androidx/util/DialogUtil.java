f09b9d73821bbc11e93305436b0427164030a36a
Mike Jennings <jennings@google.com>
2008-10-25 18:29:01 +0000

    Fixed DialogUtil
    git-svn-id: https://android-lastfm.googlecode.com/svn/trunk@57 a9466a9e-ad51-0410-a212-537f35f33a4e

--- src/androidx/util/DialogUtil.java
+++ src/androidx/util/DialogUtil.java
@@ -1,22 +1,19 @@
 package androidx.util;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 
 public class DialogUtil {
 	private DialogUtil() {
 	}
 
-	public static void showAlertDialog(Activity activity, int resTitle, int resMessage, int resIcon, long millisToShow) {
-		Dialog alert = (new AlertDialog.Builder(activity).setTitle(
+	public static void showAlertDialog(Context context, int resTitle, int resMessage, int resIcon, long millisToShow) {
+		Dialog alert = (new AlertDialog.Builder(context).setTitle(
 				resTitle).setIcon(resIcon)
 				.setMessage(resMessage)).create();
 		alert.show();
 		// dismiss the alert dialog after 2 seconds
 		GUITaskQueue.getInstance().addTask(new DismissLaterTask(alert, millisToShow));
-		// call finish on this activity after the alert dialog is dismissed
-		
-		GUITaskQueue.getInstance().addTask(new FinishLaterTask(activity, Activity.RESULT_CANCELED, 0));
 	}
 }
