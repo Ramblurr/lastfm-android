e02549dd7ae1e235519aad00279c108437ce0ea0
Mike Jennings <jennings@google.com>
2008-10-26 17:28:09 +0000

    Refactored LastfmRadio
    git-svn-id: https://android-lastfm.googlecode.com/svn/trunk@68 a9466a9e-ad51-0410-a212-537f35f33a4e

--- src/fm/last/LastfmRadio.java
+++ src/fm/last/LastfmRadio.java
@@ -1,8 +1,11 @@
 package fm.last;
 
+import android.net.Uri;
+import androidx.util.GUITaskQueue;
 import androidx.util.ResultReceiver;
 import fm.last.api.Session;
 import fm.last.api.Station;
+import fm.last.tasks.TuneRadioTask;
 
 public class LastfmRadio implements ResultReceiver<Session> {
 	private static LastfmRadio instance;
@@ -15,10 +18,12 @@ public class LastfmRadio implements ResultReceiver<Session> {
 	}
 
 	private Session session;
+	private Station currentStation;
 	
 	
 	private ResultReceiver<Station> stationResult = new ResultReceiver<Station>() {
 		public void resultObtained(Station station) {
+			setCurrentStation(station);
 		}
 		
 		public void handle_exception(Throwable t) {
@@ -28,6 +33,14 @@ public class LastfmRadio implements ResultReceiver<Session> {
 	private LastfmRadio() {
 	}
 	
+	private void setCurrentStation(Station station) {
+		currentStation = station;
+	}
+	
+	public Station getCurrentStation() {
+		return currentStation;
+	}
+	
 	public void setSession(Session session) {
 		this.session = session;
 	}
@@ -35,6 +48,12 @@ public class LastfmRadio implements ResultReceiver<Session> {
 	public Session getSession() {
 		return session;
 	}
+	
+	public void tuneToSimilarArtist(String artist) {
+		String station = "lastfm://artist/" + Uri.encode( artist ) + "/similarartists";
+		GUITaskQueue.getInstance().addTask(new TuneRadioTask(station, stationResult));
+	}
+
 	
 	public void resultObtained(Session session) {
 		this.session = session;
