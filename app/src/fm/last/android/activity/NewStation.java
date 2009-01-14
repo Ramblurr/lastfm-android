66f19114cb474623a6d18746567c84bf1e4ac43d
Sam Steele <sam@last.fm>
2009-01-14 15:30:01 +0000

    Refactor NewStation to use UserTask objects for search. Enable user search. Add mattb's new station starter background
    git-svn-id: https://android-lastfm.googlecode.com/svn/trunk@259 a9466a9e-ad51-0410-a212-537f35f33a4e

--- app/src/fm/last/android/activity/NewStation.java
+++ app/src/fm/last/android/activity/NewStation.java
@@ -11,6 +11,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -23,6 +24,7 @@ import android.view.Window;
 import android.view.View.OnClickListener;
 import android.view.View.OnKeyListener;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
@@ -36,6 +38,7 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import fm.last.android.AndroidLastFmServerFactory;
@@ -45,12 +48,17 @@ import fm.last.android.R;
 import fm.last.android.R.id;
 import fm.last.android.R.layout;
 import fm.last.android.adapter.LastFMStreamAdapter;
+import fm.last.android.adapter.ListAdapter;
+import fm.last.android.adapter.ListEntry;
+import fm.last.android.utils.ImageCache;
+import fm.last.android.utils.UserTask;
 import fm.last.android.widget.TabBar;
 import fm.last.android.widget.TabBarListener;
 import fm.last.api.LastFmServer;
 import fm.last.api.Session;
 import fm.last.api.Artist;
 import fm.last.api.Tag;
+import fm.last.api.User;
 
 public class NewStation extends ListActivity implements TabBarListener
 {
@@ -65,9 +73,11 @@ public class NewStation extends ListActivity implements TabBarListener
     
     private SearchType searching;
     private EditText searchBar;
-    private LastFMStreamAdapter mAdapter;
+    private ListAdapter mAdapter;
     private boolean mDoingSearch;
     private TabBar mTabBar;
+	private ImageCache mImageCache;
+	private TextView mHint;
     
     private final int TAB_ARTIST = 0;
     private final int TAB_TAG = 1;
@@ -83,6 +93,8 @@ public class NewStation extends ListActivity implements TabBarListener
 
         searchBar = ( EditText ) findViewById( R.id.station_editbox );
         findViewById( R.id.search ).setOnClickListener( mNewStation );
+        
+        mHint = (TextView)findViewById(R.id.search_hint);
 
 		mTabBar = (TabBar) findViewById(R.id.TabBar);
 		mTabBar.setListener(this);
@@ -90,22 +102,25 @@ public class NewStation extends ListActivity implements TabBarListener
 		mTabBar.addTab("Tag", TAB_TAG);
 		mTabBar.addTab("User", TAB_USER);
 		mTabBar.setActive(TAB_ARTIST);
-		//tabChanged("Artist", TAB_ARTIST);
+		tabChanged(TAB_ARTIST);
         
         if ( searching == null )
             mTabBar.setActive(TAB_ARTIST);
 
-        mAdapter = new LastFMStreamAdapter( this );
-        setListAdapter( mAdapter );
+        mImageCache = new ImageCache();
+        
 		getListView().setOnItemSelectedListener(new OnListRowSelectedListener(getListView()));
         mDoingSearch = false;
     }
 
     @Override
-    public void onStop()
+    public void onResume()
     {
-    	super.onStop();
-        mAdapter.updateModel();
+    	if(mAdapter != null) {
+    		mAdapter.enableLoadBar(-1);
+    		mAdapter.notifyDataSetInvalidated();
+    	}
+    	super.onResume();
     }
     
 	public void tabChanged(int index) {
@@ -113,16 +128,19 @@ public class NewStation extends ListActivity implements TabBarListener
         {
             searching = SearchType.Artist;
             searchBar.setHint( "Enter an Artist" );
+            mHint.setText(getResources().getString(R.string.newstation_hint_artist));
         }
         else if ( index == TAB_TAG )
         {
             searching = SearchType.Tag;
             searchBar.setHint( "Enter a Tag" );
+            mHint.setText(getResources().getString(R.string.newstation_hint_tag));
         }
         else if ( index == TAB_USER )
         {
             searching = SearchType.User;
             searchBar.setHint( "Enter a Username" );
+            mHint.setText(getResources().getString(R.string.newstation_hint_user));
         }
 	}
 
@@ -131,145 +149,207 @@ public class NewStation extends ListActivity implements TabBarListener
 
         public void onClick( View v )
         {
-	        final String txt = ( ( EditText ) findViewById( R.id.station_editbox ) )
-	                .getText().toString();
-	        final Handler uiThreadCallback = new Handler();
-	        final Runnable runInUIThread = new Runnable()
-	        {
-	
-	            public void run()
-	            {
-	
-	                updateList();
-	            }
-	        };
-	        final Session session = ( Session ) LastFMApplication
-	                .getInstance().map.get( "lastfm_session" );
-	        if ( searching == SearchType.Artist )
-	        {
-	            mDoingSearch = true;
-	            new Thread()
-	            {
-	
-	                @Override
-	                public void run()
-	                {
-	
-	                    _searchArtist( txt, session );
-	                    uiThreadCallback.post( runInUIThread );
-	                }
-	            }.start();
-	            Toast.makeText( NewStation.this, "Searching...",
-	                    Toast.LENGTH_LONG ).show();
-	        }
-	        else if ( searching == SearchType.Tag )
-	        {
-	            mDoingSearch = true;
-	            new Thread()
-	            {
-	
-	                @Override
-	                public void run()
-	                {
-	
-	                    _searchTag( txt, session );
-	                    uiThreadCallback.post( runInUIThread );
-	                }
-	            }.start();
-	            Toast.makeText( NewStation.this, "Searching...",
-	                    Toast.LENGTH_LONG ).show();
-	        }
-	        else if ( searching == SearchType.User )
-	        {
-	            boolean exists = false;//User.userExists( txt, session.getApiKey() );
-	            if ( !exists )
-	            {
-	                AlertDialog.Builder error = new AlertDialog.Builder(
-	                        NewStation.this );
-	                error.setTitle( "Error" ).setMessage(
-	                        "No such user '" + txt + "'." )
-	                        .setPositiveButton( "OK", null ).show();
-	                return;
-	            }
-	            else
-	            {
-	                mAdapter.resetList();
-	                /*Radio.RadioStation r = Radio.RadioStation
-	                        .personal( txt );
-	                mAdapter.putStation( txt + "'s Library", r );*/
-	                updateList();
-	            }
-	        }
+			if(searching == SearchType.Artist ) {
+				new SearchArtistsTask().execute((Void)null);
+			}
+			if(searching == SearchType.Tag ) {
+				new SearchTagsTask().execute((Void)null);
+			}
+			if(searching == SearchType.User ) {
+				new SearchUsersTask().execute((Void)null);
+			}
         }
     };
-
-    private void updateList()
-    {
-
-        mDoingSearch = false;
-        mAdapter.updateModel();
-    }
-
-    private void _searchTag( String atag, Session session )
-    {
-        LastFmServer server = AndroidLastFmServerFactory.getServer();
-        Tag[] results;
-		try {
-			mTags = server.searchForTag( atag );
-
-	        runOnUiThread(updateSearchResults);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    }
-
-    private void _searchArtist( String name, Session session )
-    {
-        LastFmServer server = AndroidLastFmServerFactory.getServer();
-        Artist[] results;
-		try {
-			mArtists = server.searchForArtist( name );
-	        
-	        runOnUiThread(updateSearchResults);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    }
     
-    private Runnable updateSearchResults = new Runnable() {
+    private class SearchTagsTask extends UserTask<Void, Void, Boolean> {
     	
-    	public void run() {
-    		mAdapter.resetList();
-    	    if( searching == SearchType.Artist) {
-    	        for ( Artist artist : mArtists )
-	            {
-    	            System.out.println("Artist can stream? " + artist.getStreamable());
-    	            if ( artist.getStreamable().equals("1") ) {
-    	                System.out.printf("Search match: %s\n", artist.getName());
-    	                mAdapter.putStation( artist.getName(), "lastfm://artist/" + Uri.encode(artist.getName()) + "/similarartists" );
-	                }
-	            }
-	        } else if( searching == SearchType.Tag) {
-	            for ( Tag tag : mTags )
-                {
-	                mAdapter.putStation( tag.getName(), "lastfm://globaltags/" + Uri.encode(tag.getName()) );
-                }
-    		} else if( searching == SearchType.User) {
-    			// TODO search result parsing for users
-    		}
-    		mAdapter.updateModel();
-    		getListView().setVisibility(View.VISIBLE);
-    		findViewById(R.id.search_hint).setVisibility(View.GONE);
-    	}
-    };
+        @Override
+    	public void onPreExecute() {
+            mDoingSearch = true;
+            Toast.makeText( NewStation.this, "Searching...",
+                    Toast.LENGTH_LONG ).show();
+        }
+    	
+        @Override
+        public Boolean doInBackground(Void...params) {
+	        final String txt = ( ( EditText ) findViewById( R.id.station_editbox ) )
+            	.getText().toString();
+	        final Session session = ( Session ) LastFMApplication
+            	.getInstance().map.get( "lastfm_session" );
+	        LastFmServer server = AndroidLastFmServerFactory.getServer();
+            boolean success = false;
+            mAdapter = new ListAdapter(NewStation.this, mImageCache);
+			getListView().setOnItemClickListener(new OnItemClickListener() {
 
-    public void onListItemClick( ListView l, View v, int position, long id ) {
-    	l.getOnItemSelectedListener().onItemSelected(l, v, position, id);
-    	ViewSwitcher switcher = (ViewSwitcher)v.findViewById(R.id.row_view_switcher);
-    	switcher.showNext();
-    	LastFMApplication.getInstance().playRadioStation(this, mAdapter.getStation(position));
+				public void onItemClick(AdapterView<?> l, View v,
+						int position, long id) {
+					Tag tag = (Tag)mAdapter.getItem(position);
+					mAdapter.enableLoadBar(position);
+			    	LastFMApplication.getInstance().playRadioStation(NewStation.this, "lastfm://globaltags/"+Uri.encode(tag.getName()));
+				}
+				
+			});
+            
+    		try {
+   				mTags = server.searchForTag( txt );
+    			ArrayList<ListEntry> iconifiedEntries = new ArrayList<ListEntry>();
+    			for(int i=0; i< ((mTags.length < 10) ? mTags.length : 10); i++){
+    				ListEntry entry = new ListEntry(mTags[i], 
+    						R.drawable.tag_dark, 
+    						mTags[i].getName(), 
+    						R.drawable.radio_icon);
+    				iconifiedEntries.add(entry);
+    			}
+    			mAdapter.setSourceIconified(iconifiedEntries);
+    			success = true;
+    		} catch (IOException e) {
+    			e.printStackTrace();
+    		}
+            return success;
+        }
+
+        @Override
+        public void onPostExecute(Boolean result) {
+        	if(result) {
+                setListAdapter( mAdapter );
+        		getListView().setVisibility(View.VISIBLE);
+        		findViewById(R.id.search_hint).setVisibility(View.GONE);
+        	} else {
+        		Toast.makeText(NewStation.this, "No tags found", Toast.LENGTH_SHORT);
+        		getListView().setVisibility(View.GONE);
+        		findViewById(R.id.search_hint).setVisibility(View.VISIBLE);
+        	}
+        }
+    }
+
+    private class SearchArtistsTask extends UserTask<Void, Void, Boolean> {
+    	
+        @Override
+    	public void onPreExecute() {
+            mDoingSearch = true;
+            Toast.makeText( NewStation.this, "Searching...",
+                    Toast.LENGTH_LONG ).show();
+        }
+    	
+        @Override
+        public Boolean doInBackground(Void...params) {
+	        final String txt = ( ( EditText ) findViewById( R.id.station_editbox ) )
+            	.getText().toString();
+	        final Session session = ( Session ) LastFMApplication
+            	.getInstance().map.get( "lastfm_session" );
+	        LastFmServer server = AndroidLastFmServerFactory.getServer();
+            boolean success = false;
+            mAdapter = new ListAdapter(NewStation.this, mImageCache);
+			getListView().setOnItemClickListener(new OnItemClickListener() {
+
+				public void onItemClick(AdapterView<?> l, View v,
+						int position, long id) {
+					Artist artist = (Artist)mAdapter.getItem(position);
+					mAdapter.enableLoadBar(position);
+			    	LastFMApplication.getInstance().playRadioStation(NewStation.this, "lastfm://artist/"+Uri.encode(artist.getName())+"/similarartists");
+				}
+				
+			});
+            
+    		try {
+   				mArtists = server.searchForArtist( txt );
+    			ArrayList<ListEntry> iconifiedEntries = new ArrayList<ListEntry>();
+    			for(int i=0; i< ((mArtists.length < 10) ? mArtists.length : 10); i++){
+    	            if ( mArtists[i].getStreamable().equals("1") ) {
+	    				ListEntry entry = new ListEntry(mArtists[i], 
+	    						R.drawable.tag_dark, 
+	    						mArtists[i].getName(), 
+	    						mArtists[i].getImages()[0].getUrl(),
+	    						R.drawable.radio_icon);
+	    				iconifiedEntries.add(entry);
+    	            }
+    			}
+    			if(iconifiedEntries.size() > 0) {
+	    			mAdapter.setSourceIconified(iconifiedEntries);
+	    			success = true;
+    			}
+    		} catch (IOException e) {
+    			e.printStackTrace();
+    		}
+            return success;
+        }
+
+        @Override
+        public void onPostExecute(Boolean result) {
+        	if(result) {
+                setListAdapter( mAdapter );
+        		getListView().setVisibility(View.VISIBLE);
+        		findViewById(R.id.search_hint).setVisibility(View.GONE);
+        	} else {
+        		Toast.makeText(NewStation.this, "No artists found", Toast.LENGTH_SHORT);
+        		getListView().setVisibility(View.GONE);
+        		findViewById(R.id.search_hint).setVisibility(View.VISIBLE);
+        	}
+        }
+    }
+
+    private class SearchUsersTask extends UserTask<Void, Void, Boolean> {
+    	
+        @Override
+    	public void onPreExecute() {
+            mDoingSearch = true;
+            Toast.makeText( NewStation.this, "Searching...",
+                    Toast.LENGTH_LONG ).show();
+        }
+    	
+        @Override
+        public Boolean doInBackground(Void...params) {
+	        final String txt = ( ( EditText ) findViewById( R.id.station_editbox ) )
+            	.getText().toString();
+	        final Session session = ( Session ) LastFMApplication
+            	.getInstance().map.get( "lastfm_session" );
+	        LastFmServer server = AndroidLastFmServerFactory.getServer();
+            boolean success = false;
+            mAdapter = new ListAdapter(NewStation.this, mImageCache);
+			getListView().setOnItemClickListener(new OnItemClickListener() {
+
+				public void onItemClick(AdapterView<?> l, View v,
+						int position, long id) {
+					User user = (User)mAdapter.getItem(position);
+					mAdapter.enableLoadBar(position);
+                    Intent profileIntent = new Intent(NewStation.this, fm.last.android.activity.Profile.class);
+                    profileIntent.putExtra("lastfm.profile.username", user.getName());
+                    startActivity(profileIntent);
+				}
+				
+			});
+            
+    		try {
+   				User user = server.getAnyUserInfo( txt );
+   				if(user != null) {
+	    			ArrayList<ListEntry> iconifiedEntries = new ArrayList<ListEntry>();
+					ListEntry entry = new ListEntry(user, 
+							R.drawable.artist_icon, 
+							user.getName(),
+							user.getImages().length == 0 ? "" : user.getImages()[0].getUrl(),
+							R.drawable.arrow);
+					iconifiedEntries.add(entry);
+	    			mAdapter.setSourceIconified(iconifiedEntries);
+	    			success = true;
+   				}
+    		} catch (IOException e) {
+    			e.printStackTrace();
+    		}
+            return success;
+        }
+
+        @Override
+        public void onPostExecute(Boolean result) {
+        	if(result) {
+                setListAdapter( mAdapter );
+        		getListView().setVisibility(View.VISIBLE);
+        		findViewById(R.id.search_hint).setVisibility(View.GONE);
+        	} else {
+        		Toast.makeText(NewStation.this, "No users found", Toast.LENGTH_SHORT);
+        		getListView().setVisibility(View.GONE);
+        		findViewById(R.id.search_hint).setVisibility(View.VISIBLE);
+        	}
+        }
     }
 
 }
