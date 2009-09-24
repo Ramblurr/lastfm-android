8229b460e0578ed3d0a87ab24218bced9d7af088
Sam Steele <sam@last.fm>
2009-09-24 10:58:58 -0700

    refresh subscriber status in the background so we don't slow down the app launch

--- app/src/fm/last/android/activity/Profile.java
+++ app/src/fm/last/android/activity/Profile.java
@@ -162,25 +162,7 @@ public class Profile extends ListActivity
         mUsername = getIntent().getStringExtra("lastfm.profile.username");
         if( mUsername == null ) {
             mUsername = session.getName();
-            String subscriber = session.getSubscriber();
             isAuthenticatedUser = true;
-            //Check our subscriber status
-            LastFmServer server = AndroidLastFmServerFactory.getServer();
-            try {
-				User user = server.getUserInfo(session.getKey());
-				if(user != null) {
-					subscriber = user.getSubscriber();
-			        SharedPreferences settings = getSharedPreferences( LastFm.PREFS, 0 );
-		            SharedPreferences.Editor editor = settings.edit();
-		            editor.putString( "lastfm_subscriber", subscriber);
-		            editor.commit();
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	    	session = new Session(session.getName(), session.getKey(), subscriber);
-	        LastFMApplication.getInstance().map.put( "lastfm_session", session );
         } 
         else 
             isAuthenticatedUser = false;
@@ -357,6 +339,23 @@ public class Profile extends ListActivity
             Session session = LastFMApplication.getInstance().map.get( "lastfm_session" );
             LastFMApplication.getInstance().fetchRecentStations();
             SetupRecentStations();
+            //Check our subscriber status
+            LastFmServer server = AndroidLastFmServerFactory.getServer();
+            try {
+				User user = server.getUserInfo(session.getKey());
+				if(user != null) {
+					String subscriber = user.getSubscriber();
+			        SharedPreferences settings = getSharedPreferences( LastFm.PREFS, 0 );
+		            SharedPreferences.Editor editor = settings.edit();
+		            editor.putString( "lastfm_subscriber", subscriber);
+		            editor.commit();
+			    	session = new Session(session.getName(), session.getKey(), subscriber);
+			        LastFMApplication.getInstance().map.put( "lastfm_session", session );
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
     		try {
     		    if( mUsername == null) {
     				mUser = mServer.getUserInfo( session.getKey() );
