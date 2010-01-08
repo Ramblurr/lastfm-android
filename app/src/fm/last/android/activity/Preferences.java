ac43eb08dd3173ec8f6a262bd30f691ccfa7f044
Sam Steele <sam@last.fm>
2010-01-08 09:25:53 -0800

    Clear the scrobbling notification when toggling the scrobbling preference

--- app/src/fm/last/android/activity/Preferences.java
+++ app/src/fm/last/android/activity/Preferences.java
@@ -5,7 +5,9 @@ package fm.last.android.activity;
 
 import fm.last.android.LastFMApplication;
 import fm.last.android.R;
+import android.app.NotificationManager;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
 /**
@@ -16,8 +18,19 @@ public class Preferences extends PreferenceActivity {
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
     	addPreferencesFromResource(R.xml.preferences);
+    	
+    	findPreference("scrobble").setOnPreferenceChangeListener(scrobbletoggle);
+    	findPreference("scrobble_music_player").setOnPreferenceChangeListener(scrobbletoggle);
 	}
 
+	Preference.OnPreferenceChangeListener scrobbletoggle = new Preference.OnPreferenceChangeListener() {
+		public boolean onPreferenceChange(Preference preference, Object newValue) {
+			NotificationManager nm = ( NotificationManager ) getSystemService( NOTIFICATION_SERVICE );
+			nm.cancel( 1338 );
+			return true;
+		}
+	};
+	
 	@Override
 	public void onResume() {
 		super.onResume();
