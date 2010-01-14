1fbd4d0aca1064813300732f35b74892c1300cef
Sam Steele <sam@last.fm>
2010-01-14 13:56:15 -0800

    Handle loving and banning radio tracks when scrobbling is disabled

--- app/src/fm/last/android/scrobbler/MusicIntentReceiver.java
+++ app/src/fm/last/android/scrobbler/MusicIntentReceiver.java
@@ -24,14 +24,23 @@ import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.util.concurrent.ArrayBlockingQueue;
 
+import fm.last.android.AndroidLastFmServerFactory;
 import fm.last.android.LastFMApplication;
+import fm.last.android.R;
+import fm.last.android.activity.Profile;
+import fm.last.android.player.IRadioPlayer;
+import fm.last.android.player.RadioPlayerService;
+import fm.last.api.LastFmServer;
 import fm.last.api.Session;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * @author sam
@@ -76,6 +85,46 @@ public class MusicIntentReceiver extends BroadcastReceiver {
 	        out.setAction(intent.getAction());
 	        out.putExtras(intent);
 	        context.startService(out);
+        } else if ( s != null && s.getKey().length() > 0 && intent.getAction().equals("fm.last.android.LOVE")) {
+        	IBinder service = peekService(context, new Intent(context,RadioPlayerService.class));
+    		if (service == null){
+    			return;
+    		}
+   			try {
+   	   			IRadioPlayer player = fm.last.android.player.IRadioPlayer.Stub.asInterface( service );
+				if(player != null && player.isPlaying()) {
+					String track = player.getTrackName();
+					String artist = player.getArtistName();
+					if(!track.equals(RadioPlayerService.UNKNOWN) && !artist.equals(RadioPlayerService.UNKNOWN)) {
+	    				LastFmServer server = AndroidLastFmServerFactory.getServer();
+						server.loveTrack(artist, track, LastFMApplication.getInstance().session.getKey());
+						Toast.makeText(context, context.getString(R.string.scrobbler_trackloved), Toast.LENGTH_SHORT).show();
+					}
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        } else if ( s != null && s.getKey().length() > 0 && intent.getAction().equals("fm.last.android.BAN")) {
+        	IBinder service = peekService(context, new Intent(context,RadioPlayerService.class));
+    		if (service == null){
+    			return;
+    		}
+   			try {
+   	   			IRadioPlayer player = fm.last.android.player.IRadioPlayer.Stub.asInterface( service );
+				if(player != null && player.isPlaying()) {
+					String track = player.getTrackName();
+					String artist = player.getArtistName();
+					if(!track.equals(RadioPlayerService.UNKNOWN) && !artist.equals(RadioPlayerService.UNKNOWN)) {
+	    				LastFmServer server = AndroidLastFmServerFactory.getServer();
+						server.banTrack(artist, track, LastFMApplication.getInstance().session.getKey());
+						Toast.makeText(context, context.getString(R.string.scrobbler_trackbanned), Toast.LENGTH_SHORT).show();
+					}
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
         }
 	}
 }
